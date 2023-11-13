c7cc87b2916cd652b15f272cb8020f2e63f0fbd5
Syaroful <syaroful10403@gmail.com>
2023-11-13 14:25:03 +0700

    feat: added app asset class

---
+++ lib/presentation/theme/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  static const String id_card = 'assets/images/id_card.png';
+  static const String face_id = 'assets/images/face_id.png';
+  static const String icon_home = 'assets/images/icon_home.png';
+  static const String icon_history = 'assets/images/icon_history.png';
+  static const String icon_setting = 'assets/images/icon_setting.png';
+  static const String icon_scan = 'assets/images/icon_scan.png';
+  static const String icon_gallery = 'assets/images/icon_gallery.png';
+  static const String icon_files = 'assets/images/icon_files.png';
+  static const String arrow_right = 'assets/images/arrow_right.png';
+}
