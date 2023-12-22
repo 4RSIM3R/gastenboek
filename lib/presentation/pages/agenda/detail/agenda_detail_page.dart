d65543f037a65573b83f83d8b5df8d739c4ba966
Syaroful <syaroful10403@gmail.com>
2023-12-22 19:41:14 +0700

    feat: hand-off to ilzam again

--- lib/presentation/pages/agenda/detail/agenda_detail_page.dart
+++ lib/presentation/pages/agenda/detail/agenda_detail_page.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:next_starter/data/models/agenda/agenda_model.dart';
-import 'package:next_starter/presentation/components/button/primary_button.dart';
 import 'package:next_starter/presentation/pages/guest/guest_list.dart';
-import 'package:next_starter/presentation/routes/app_router.dart';
 import 'package:next_starter/presentation/theme/theme.dart';
 
 @RoutePage()
@@ -21,81 +19,90 @@ class AgendaDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Detail Event'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                    'https://images.unsplash.com/photo-1607013407627-6ee814329547?q=80&w=2764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'March 21, 2023',
-              style: AppStyles.text12PxMedium.copyWith(color: Colors.grey),
-            ),
-            Text(
-              model.name ?? 'Wisuda Angkatan 2023 - JTI Polinema',
-              style: AppStyles.text18Px,
-            ),
-            SizedBox(height: 12),
-            Text(
-              'ini nanti ada deskripsi eventnya',
-              style: AppStyles.text12Px.copyWith(color: Colors.grey),
-            ),
-            SizedBox(height: 12),
-            Row(
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  CupertinoIcons.location,
-                  size: 16,
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                        'https://images.unsplash.com/photo-1607013407627-6ee814329547?q=80&w=2764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                        fit: BoxFit.cover),
+                  ),
                 ),
-                SizedBox(
-                  width: 8,
+                SizedBox(height: 16),
+                Text(
+                  'March 21, 2023',
+                  style: AppStyles.text12PxMedium.copyWith(color: Colors.grey),
                 ),
                 Text(
-                  'Gedung Serba Guna Polinema',
-                  style: AppStyles.text12Px,
-                )
+                  model.name ?? 'Wisuda Angkatan 2023 - JTI Polinema',
+                  style: AppStyles.text18Px,
+                ),
+                SizedBox(height: 12),
+                Text(
+                  'ini nanti ada deskripsi eventnya',
+                  style: AppStyles.text12Px.copyWith(color: Colors.grey),
+                ),
+                SizedBox(height: 12),
+                Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.location,
+                      size: 16,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      'Gedung Serba Guna Polinema',
+                      style: AppStyles.text12Px,
+                    )
+                  ],
+                ),
+                SizedBox(height: 12),
+                Divider(
+                  thickness: 2,
+                  color: Colors.grey[300],
+                ),
+                SizedBox(height: 12),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[300],
+                  ),
+                  child: FlutterMap(
+                      options: MapOptions(
+                        initialCenter: LatLng(-7.946639, 112.615876),
+                        initialZoom: 16,
+                      ),
+                      children: [
+                        TileLayer(
+                          urlTemplate:
+                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          userAgentPackageName: 'com.example.app',
+                        )
+                      ]),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
               ],
             ),
-            SizedBox(height: 12),
-            Divider(
-              thickness: 2,
-              color: Colors.grey[300],
-            ),
-            SizedBox(height: 12),
-            Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.grey[300],
-              ),
-              child: FlutterMap(
-                  options: MapOptions(
-                    initialCenter: LatLng(-7.946639, 112.615876),
-                    initialZoom: 16,
-                  ),
-                  children: [
-                    TileLayer(
-                      urlTemplate:
-                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      userAgentPackageName: 'com.example.app',
-                    )
-                  ]),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            InkWell(
+          ),
+          Positioned(
+            bottom: 24,
+            right: 0,
+            left: 0,
+            child: InkWell(
               onTap: () {
                 Navigator.push(
                   context,
@@ -103,6 +110,7 @@ class AgendaDetailPage extends StatelessWidget {
                 );
               },
               child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 18),
                 height: 50,
                 width: double.infinity,
                 decoration: BoxDecoration(
@@ -110,14 +118,16 @@ class AgendaDetailPage extends StatelessWidget {
                   color: ColorTheme.primary2,
                 ),
                 child: Center(
-                  child: Text('Check Attendance',
-                      style: AppStyles.text14PxSemiBold
-                          .copyWith(color: Colors.white)),
+                  child: Text(
+                    'Check Attendance',
+                    style: AppStyles.text14PxSemiBold
+                        .copyWith(color: Colors.white),
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/presentation/pages/guest/guest_list.dart
+++ lib/presentation/pages/guest/guest_list.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:next_starter/presentation/routes/app_router.dart';
@@ -20,17 +21,30 @@ class _GuestListState extends State<GuestList> {
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Elang Putra Adam'),
-            subtitle: Text('3520253735242'),
-            trailing: Text('Status'),
+          return Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                bottom: BorderSide(color: Colors.grey.shade200),
+              ),
+            ),
+            child: ListTile(
+              leading: Icon(CupertinoIcons.person_alt),
+              title: Text(
+                'Elang Putra Adam',
+                style: AppStyles.text12PxMedium,
+              ),
+              subtitle: Text('3520253735242'),
+              trailing: Text('Status'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: ColorTheme.primary2,
         onPressed: () {
-          // context.route.push(const ScanRoute());
+          context.router.push(const ScanRoute());
         },
         child: Icon(CupertinoIcons.add),
       ),
